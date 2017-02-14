805e469ddb925c474e220ef0b10054d3587a71fc
CH33ZED <zlin3@stuy.edu>
2017-02-14 14:52:20 -0500

    stuff

--- canvas.js
+++ canvas.js
@@ -9,7 +9,13 @@
                var d = event.clientY;  
                ctx.fillRect(c, d, 100, 100);
             }
-            a.addEventListener('click', draw);
+            var draw2 = function(event){
+                var e = event.clientX;
+		var f = event.clientY;
+	        ctx.arc(e, f, 10, 0, 360);
+		ctx.stroke();
+            }
+            a.addEventListener('click', draw2);
             var clean = function(e){
                ctx.clearRect(0,0,1000,1000);
             }
