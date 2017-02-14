3ffd0fb22c994ae5da152d0bfde56cc380c0a08c
CH33ZED <zlin3@stuy.edu>
2017-02-13 23:14:28 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 ﻿
             var a = document.getElementById("slate");
             var b = document.getElementById("clear");
-            Var ctx = a.getContext(‘2d’); 
+            var ctx = a.getContext(‘2d’); 
             var draw = function(event){       
                var c = event.clientX;
                var d = event.clientY;  
