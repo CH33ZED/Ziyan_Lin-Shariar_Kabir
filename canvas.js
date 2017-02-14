b838f329184382cb2abcf17ab51ec7d1286001ed
CH33ZED <zlin3@stuy.edu>
2017-02-13 23:12:46 -0500

    stuff

---
+++ canvas.js
@@ -0,0 +1,15 @@
+﻿
+            var a = document.getElementById("slate");
+            var b = document.getElementById("clear");
+            Var ctx = a.getContext(‘2d’); 
+            var draw = function(event){       
+               var c = event.clientX;
+               var d = event.clientY;  
+               ctx.fillRect(c, d, 100, 100)
+            }
+            a.addEventListener('click', draw);
+            var clean = function(e){
+               ctx.clearRect(0,0,1000,1000);
+            }
+            b.addEventListener('click', clean);
+ 
